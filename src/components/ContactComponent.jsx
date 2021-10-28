9cd6499c86b6bfdceae4a5e6709153dfeecc1881
Ben Daniels <benjamindaniels21@gmail.com>
2021-10-27 21:12:31 -0400

    React Redux Forms Revisited

--- src/components/ContactComponent.jsx
+++ src/components/ContactComponent.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { Breadcrumb, BreadcrumbItem, Button, Label, Col, Row } from 'reactstrap';
 import { Link } from "react-router-dom";
-import { Control, LocalForm, Errors } from 'react-redux-form';
+import { Control, Form, Errors, actions } from 'react-redux-form';
 
 
 const required = val => val && val.length;
@@ -47,6 +47,7 @@ class Contact extends Component {
    handleSubmit(values) {
         console.log("Current state is: " + JSON.stringify(values));
         alert("Current state is: " + JSON.stringify(values));
+        this.props.resetFeedbackForm();
     }
 
 
@@ -105,7 +106,7 @@ class Contact extends Component {
                         <hr />
                     </div>
                     <div className="col-md-10">
-                     <LocalForm onSubmit={values => this.handleSubmit(values)}>
+                     <Form model= "feedbackForm" onSubmit={values => this.handleSubmit(values)}>
                             <Row className="form-group">
                                 <Label htmlFor="firstName" md={2}>First Name</Label>
                                 <Col md={10}>
@@ -244,7 +245,7 @@ class Contact extends Component {
                                     </Button>
                                 </Col>
                             </Row>
-                        </LocalForm> 
+                        </Form> 
                     </div>
                 </div>
       </div> 
--- src/components/MainComponent.jsx
+++ src/components/MainComponent.jsx
@@ -9,10 +9,14 @@ import Contact from "./ContactComponent";
 import { addComment, fetchCampsites } from '../redux/ActionCreators';
 import { Switch, Route, Redirect, withRouter } from "react-router-dom";
 import { connect } from 'react-redux';
+import { actions } from 'react-redux-form';
+
 
 const mapDispatchToProps = {
     addComment: (campsiteId, rating, author, text) => (addComment(campsiteId, rating, author, text)),
-    fetchCampsites: () => (fetchCampsites())
+    fetchCampsites: () => (fetchCampsites()),
+    fetchCampsites: () => (fetchCampsites()),
+    resetFeedbackForm: () => (actions.reset('feedbackForm'))
 };
 
 const mapStateToProps = state => {
@@ -67,7 +71,7 @@ class Main extends Component {
             render={() => <Directory campsites={this.props.campsites} />}
           />
           <Route path="/directory/:campsiteId" component={CampsiteWithId} />
-          <Route exact path="/contactus" component={Contact} />
+          <Route exact path="/contactus" render={()=> <Contact resetFeedbackForm = {this.props.resetFeedbackForm} />} />
           <Redirect to="/home" />
         </Switch>
         <Footer />
