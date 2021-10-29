9beae7c487352c18d8f7fcf955e823b7ebf2fd1f
Ben Daniels <benjamindaniels21@gmail.com>
2021-10-28 21:02:03 -0400

    fetch post comment

--- src/components/MainComponent.jsx
+++ src/components/MainComponent.jsx
@@ -6,14 +6,14 @@ import Footer from "./FooterComponent";
 import Home from "./HomeComponent";
 import About from "./AboutComponent";
 import Contact from "./ContactComponent";
-import { addComment, fetchCampsites, fetchComments, fetchPromotions } from '../redux/ActionCreators';
+import { postComment, fetchCampsites, fetchComments, fetchPromotions } from '../redux/ActionCreators';
 import { Switch, Route, Redirect, withRouter } from "react-router-dom";
 import { connect } from 'react-redux';
 import { actions } from 'react-redux-form';
 
 
 const mapDispatchToProps = {
-    addComment: (campsiteId, rating, author, text) => (addComment(campsiteId, rating, author, text)),
+    postComment: (campsiteId, rating, author, text) => (postComment(campsiteId, rating, author, text)),
     fetchCampsites: () => (fetchCampsites()),
     resetFeedbackForm: () => (actions.reset('feedbackForm')),
     fetchComments: () => (fetchComments()),
@@ -60,7 +60,7 @@ class Main extends Component {
         errMess={this.props.campsites.errMess}
         comments={this.props.comments.comments.filter(comment => comment.campsiteId === +match.params.campsiteId)}
         commentsErrMess={this.props.comments.errMess}
-        addComment={this.props.addComment}
+        addComment={this.props.postComment}
       />       
     );
     };
